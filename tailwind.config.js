9a7a950f6cfe9721c807e323dedc2f80a3a0248b
yvs2007 <vladimirY2007@gmail.com>
2024-10-27 19:16:39 -0400

    setup everything somewhat

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+
+    fontFamily: {
+      sans: ['Nunito', 'sans-serif'],
+    },
+    extend: {
+
+    },
   },
   variants: {
     extend: {},
