f867b03d926f2d25717951c796e809b43254dae7
yvs2007 <vladimirY2007@gmail.com>
2024-10-27 19:19:03 -0400

    touched up some things here and there
    s

--- src/components/Ribbon.vue
+++ src/components/Ribbon.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {ref} from 'vue'
+import MastCoding.png from './assests/MastCoding.png'
 
 </script>
 
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -3,5 +3,8 @@ import { ref } from 'vue'
 </script>
 
 <template>
-
+    <div class="text-center forced-colors:bg-red-500">
+<h1 class="text-5xl">MASTer Coders</h1>
+<h2>MAST Coding Club</h2>
+</div>
 </template>
