542a41b638e6f4a76ac826a2732894ceae208341
yvs2007 <vladimirY2007@gmail.com>
2024-11-06 20:01:15 -0500

    breaking up the blocks of code in the body.

---
+++ src/components/A.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div id="AEs" class="text-1xl inline-block">
+            <div id="Announcements" class="" ">
+                <h2>Announcements:</h2>
+                <br>
+                <p></p>
+            </div>
+            <div id="Events." class="">
+                <p>Events:</p>
+            </div>
+        </div> 
+</template>
--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from "vue";
+import AEs from "a.vue";
 </script>
 
 <template>
@@ -8,22 +9,17 @@ import { ref } from "vue";
         <div id="Mission" class="text-2xl text-center border-b-2 text-black">
             <h1> what is in here</h1>
         </div>
+        <div>
         <!--Announcements and Events.-->
-        <div id="AEs" class="text-1xl inline-block">
-            <div id="Announcements" class="w-[50%] border-r-2">
-                <p>Announcements:</p>
-            </div>
-            <div id="Events." class="w-[50%] ">
-                <p>Events:</p>
-            </div>
+        <AEs />
         </div> 
         <!--goals and Opportunities.-->
         <div id="GOs" class="">
             <div id="goals">
-                <p>goals</p>
+                <p>goals:</p>
             </div>
             <div id="oppourtunities">
-                <p>oppourtunities</p>
+                <p>oppourtunities:</p>
             </div>
         </div>
     </div>
